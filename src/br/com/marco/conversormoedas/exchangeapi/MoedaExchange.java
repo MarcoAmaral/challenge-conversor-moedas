e4961be81058494e8c91e6d27ea96a93cc026842
Marco Amaral <marco.amaral@gmail.com>
2025-01-04 02:34:01 -0300

    Created Record to read exchangeapi

---
+++ src/br/com/marco/conversormoedas/exchangeapi/MoedaExchange.java
@@ -0,0 +1,9 @@
+package br.com.marco.conversormoedas.exchangeapi;
+
+public record MoedaExchange(String result,
+														String base_code,
+														String target_code,
+														String time_last_update_utc,
+														double conversion_rate,
+														double conversion_result) {
+}
