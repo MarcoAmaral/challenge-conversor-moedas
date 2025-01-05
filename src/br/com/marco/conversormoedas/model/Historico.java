9b73c281cda5cc658afde9f274d1c2ec14a1ebd8
Marco Amaral <marco.amaral@gmail.com>
2025-01-04 21:39:36 -0300

    Final changes

--- src/br/com/marco/conversormoedas/model/Historico.java
+++ src/br/com/marco/conversormoedas/model/Historico.java
@@ -1,4 +1,71 @@
 package br.com.marco.conversormoedas.model;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Historico {
+		private Scanner leitura;
+		List<Moeda> historicoDeConversao = new ArrayList<>();
+
+		public Historico(Scanner leitura) {
+				this.leitura = leitura;
+		}
+
+		public void Adicionar(Moeda moeda) {
+				historicoDeConversao.add(moeda);
+		}
+
+		public void TemHistorico() {
+				if (!historicoDeConversao.isEmpty()) {
+						System.out.println("\nExibindo histórico de conversões: \n");
+						for (Moeda item : historicoDeConversao) {
+								System.out.println("- " + item);
+						}
+						QuerSalvar();
+				} else {
+						System.out.println("\nO histórico está vazio!\n");
+				}
+		}
+
+		private void QuerSalvar() {
+				leitura.nextLine(); // Tive que colocar para limpar o Scanner!!
+				String resposta = "";
+				while (!resposta.equalsIgnoreCase("2")) {
+						System.out.println("\n\nDeseja salvar o histórico?\n" +
+										"1 - Salvar!     2 - Voltar ao Menu!");
+
+						resposta = leitura.nextLine().trim();
+
+						if (resposta.equalsIgnoreCase("1")) {
+								SalvandoHistorico();
+								resposta = "2";
+
+						} else if (resposta.equalsIgnoreCase("2")) {
+								for (int i = 0; i < 15; i++) {
+										System.out.println("\n\n");
+								}
+						} else {
+								System.out.println("\nOpção inválida. Tente novamente.");
+						}
+				}
+		}
+
+		private void SalvandoHistorico() {
+				System.out.println("\nComo devo salvar o arquivo?");
+				String nomeArquivo = leitura.nextLine().trim() + ".txt";
+
+				String caminhoArquivo = System.getProperty("user.home") + "/Desktop/" + nomeArquivo;
+
+				try (FileWriter escreve = new FileWriter(caminhoArquivo)) {
+						escreve.write(historicoDeConversao.toString());
+						escreve.close();
+						System.out.println("\nArquivo salvo com sucesso em: " + caminhoArquivo);
+
+				} catch (IOException e) {
+						System.err.println("\nErro ao salvar o arquivo: " + e.getMessage());
+				}
+		}
 }
--- src/br/com/marco/conversormoedas/service/Converte.java
+++ src/br/com/marco/conversormoedas/service/Converte.java
@@ -1,11 +1,18 @@
 package br.com.marco.conversormoedas.service;
 
+import br.com.marco.conversormoedas.model.Historico;
+import br.com.marco.conversormoedas.model.Moeda;
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class Converte {
-		private String apikey = "69359ef6c55cf06c7a72fa2a";
+		private String apikey = "69359ef6c55cf06c7a72fa2a"; // This is an application for studying purposes -> This key will be deactivated soon.
 		private String url = "https://v6.exchangerate-api.com/v6/" + apikey + "/pair/";
 		private Historico historico;
 
@@ -18,4 +25,32 @@ public class Converte {
 						.setPrettyPrinting()
 						.create();
 
+		public void Converter(String moedaBase, String moedaAlvo, double valor) {
+				String urlfinal = url + moedaBase + "/" + moedaAlvo + "/" + valor;
+
+				try{
+						HttpClient client = HttpClient.newHttpClient();
+
+						HttpRequest request = HttpRequest.newBuilder()
+										.uri(URI.create(urlfinal))
+										.build();
+
+						HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+						if (response.statusCode() != 200) {
+								throw new RuntimeException("Erro na requisição: Status code " + response.statusCode());
+						}
+
+						String json = response.body();
+
+						MoedaExchange novaMoedaConvertida = gson.fromJson(json, MoedaExchange.class);
+						Moeda moeda = new Moeda(novaMoedaConvertida);
+						moeda.setValor(valor);
+						moeda.ExibeMoeda();
+						historico.Adicionar(moeda);
+
+				} catch (Exception e) {
+						throw new RuntimeException("Erro ao realizar a conversão: " + e.getMessage(), e);
+				}
+		}
 }
